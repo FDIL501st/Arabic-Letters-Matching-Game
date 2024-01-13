3709c65da1bc92ecf3fec65cf6cad3391bd38b8c
FDIL <fdil.fadilh1@gmail.com>
2024-01-12 19:42:33 -0700

    Added Game area to EasyGameView
    Added a very basic game area with spaces for the cards.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- ArabicLettersMatchingGame/Views/EasyGameView.axaml.cs
+++ ArabicLettersMatchingGame/Views/EasyGameView.axaml.cs
@@ -1,3 +1,4 @@
+using ArabicLettersMatchingGame.Models.Constants;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -9,5 +10,47 @@ public partial class EasyGameView : UserControl
     public EasyGameView()
     {
         InitializeComponent();
+        
+        // load markup file
+        AvaloniaXamlLoader.Load(this);
+        
+        // get grid so can update it 
+        var grid = EasyGameGrid;
+
+        // make grid for game
+        // will be a square, with each side have number of easy game matches number of cards/grid spaces
+        
+        // each grid space is a card
+
+        Grid gameArea = new();
+        
+        for (var i = 0; i < GameMatchNumber.Easy; i++)
+        {
+            // each grid space in game area is same size
+            gameArea.RowDefinitions.Add(new RowDefinition(GridLength.Star));
+            gameArea.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+        }
+
+        gameArea.ShowGridLines = true;
+
+#if false
+        // test data to see if gameArea works
+        TextBlock text1 = new();
+        text1.Text = "Text 1";
+        
+        gameArea.Children.Add(text1);
+        Grid.SetRow(text1,1);
+        Grid.SetColumn(text1,3);
+        
+#endif
+        
+        // add gameArea to grid and set to row 1
+        grid.Children.Add(gameArea);
+        Grid.SetRow(gameArea, 1);
+        
+        
+        // set content of view to grid, this will render the updates we added in code here
+        Content = grid;
+
     }
 }
